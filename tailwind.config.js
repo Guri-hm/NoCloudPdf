a994f815e677f3fbd1a5957cfce167f69d3685c5
Guri <f3u3emgr@yahoo.co.jp>
2025-07-24 16:41:01 +0900

    tailwindをビルドして適用 メニューボタンのスタイル調整

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+export default {
+    content: ["./**/*.razor", "./**/*.html"],
+    safelist: [
+        "divide-x",
+        "divide-gray-200"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
