ac4dec7d3960326aa3e1ca83f582837d7fe970be
Guri <f3u3emgr@yahoo.co.jp>
2025-07-28 10:01:59 +0900

    トグルボタンのコンポーネント化および汎用化

---
+++ Components/ToggleButtonItem.cs
@@ -0,0 +1,9 @@
+namespace ClientPdfApp.Components
+{
+    public class ToggleButtonItem<T>
+    {
+        public T Value { get; set; } = default!;
+        public string Label { get; set; } = "";
+        public string IconSvg { get; set; } = ""; // SVG文字列
+    }
+}
