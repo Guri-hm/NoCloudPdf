becd498271f3fa380f0603cf6c498f675d2d92c3
Guri <f3u3emgr@yahoo.co.jp>
2025-07-30 10:09:42 +0900

    googlefontのアイコンに少し移行

--- wwwroot/js/common-utils.js
+++ wwwroot/js/common-utils.js
@@ -1,4 +1,4 @@
-window.getPageType = function() {
+window.getPageType = function () {
     if (window.location.pathname.includes("/split")) return "split";
     if (window.location.pathname.includes("/merge")) return "merge";
     return "unknown";
