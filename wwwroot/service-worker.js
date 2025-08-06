37bd2fc62d12cc10ddd271abdb752f0784e3fe63
Guri <f3u3emgr@yahoo.co.jp>
2025-08-06 21:58:28 +0900

    fetch付与

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -5,3 +5,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     self.clients.claim();
 });
+self.addEventListener('fetch', event => {
+    // 通常のネットワークリクエストをそのまま通す
+    event.respondWith(fetch(event.request));
+});
