0648aaca0ccc5cb50947dfaa9f89f391938f07a3
Guri <f3u3emgr@yahoo.co.jp>
2026-01-19 22:04:41 +0900

    md未満時もボタンのラベルを表示するフラグを実装，幅に合わせるボタンの修正

--- Components/ActionButtonItem.cs
+++ Components/ActionButtonItem.cs
@@ -11,7 +11,8 @@ namespace NoCloudPdf.Components
         public EventCallback OnClick { get; set; }
         public string ButtonClass { get; set; } = "";
         public string IconPosition { get; set; } = "left"; // "left" or "right"
-        public bool StopPropagation { get; set; } = false;
         public string TextColor { get; set; } = "text-black";
+        public bool StopPropagation { get; set; } = false;
+        public bool HideLabelOnNarrow { get; set; } = true;
     }
 }
